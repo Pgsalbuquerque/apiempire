52dc578b9689d84ae6bce9a7d8e08be13081c133
Pgsalbuquerque <pgsalbuquerque@gmail.com>
2021-02-18 18:52:21 -0300

    Migration Purchases and rename some files

---
+++ src/database/migrations/20210218214823-purchase.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('purchases', {
+       id: {
+         type: Sequelize.INTEGER,
+         primaryKey: true,
+       },
+       value: {
+         type: Sequelize.FLOAT,
+         allowNull: false,
+       }
+      });
+
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('purchases');
+
+  }
+};
