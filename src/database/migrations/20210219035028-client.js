eb056091361ca87d294aae656facd8b79a9c6d4f
Pgsalbuquerque <pgsalbuquerque@gmail.com>
2021-02-19 00:55:32 -0300

    Migration clients

---
+++ src/database/migrations/20210219035028-client.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+      await queryInterface.createTable('clients', {
+         cpf: {
+           type: Sequelize.STRING,
+           primaryKey: true,
+           allowNull: false,
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          telephone1: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          telephone2: {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+          telephone3: {
+            type: Sequelize.STRING,
+            allowNull: true,
+          }
+        });
+
+  },
+
+  down: async (queryInterface) => {
+      await queryInterface.dropTable('clients');
+
+  }
+};
